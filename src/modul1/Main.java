2a8c885cc8d2f9db44380cfe35d9a4212f9632cd
jihanmaulidia09 <@jianmaulidia97@gmail.com>
2025-09-11 15:42:12 +0700

    laporan 1

---
+++ src/modul1/Main.java
@@ -0,0 +1,9 @@
+package modul1;
+
+public class Main {
+    // Fungsi khusus sebagai entry poin dari aplikasi yang dibuat.
+    public  static void main (String[] args) {
+        System.out.println("Hello Word");
+
+    }
+}
