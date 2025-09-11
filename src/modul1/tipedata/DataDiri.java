f1a62c6523e3da618694970bcccf9fc3e564f88a
jihanmaulidia09 <@jianmaulidia97@gmail.com>
2025-09-11 21:13:25 +0700

    laporan2

---
+++ src/modul1/tipedata/DataDiri.java
@@ -0,0 +1,28 @@
+package modul1.tipedata;
+
+public class DataDiri {
+    public static void main(String[] args) {
+        String teks, nama, alamat, prodi, jurusan;
+        int usia, angkatan;
+        double tinggi;
+
+        teks = "Hallo, ini adalah minggu kedua saya belajar java ";
+        nama = "Jihan Maulidia";
+        alamat = "Lhoksukon";
+        tinggi = 160;
+        usia = 19;
+        prodi = "Teknik informatika";
+        jurusan = "Teknologi Informasi dan Komputer";
+        angkatan = 2024;
+
+        System.out.println("Teks: " + teks);
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Prodi: " + prodi);
+        System.out.println("Jurusan: " + jurusan);
+        System.out.println("Usia: " + usia);
+        System.out.println("Angkatan: " + angkatan);
+
+
+    }
+}
